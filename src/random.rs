8ebcfc98bf7998afaf50a113bc40cbfe3cd8473a
Jonas Dietrich <diejoc20@htl-kaindorf.at>
2025-01-11 00:14:20 +0100

    reformatted content file, added functional random mail generation

--- src/random.rs
+++ src/random.rs
@@ -1,3 +1,36 @@
-pub fn generate_random_mail() {
-        
+use crate::util::choose_random_line;
+use crate::util::read_file;
+use crate::util::choose_multiple_random_lines;
+
+pub fn generate_random_mail() -> Result<String, Box<dyn std::error::Error>> {
+
+    let content_lines = read_file("./data/content.txt")?;
+    let subject_lines = read_file("./data/subjects.txt")?;
+    let greeting_lines = read_file("./data/greetings.txt")?;
+    let ending_lines = read_file("./data/endings.txt")?;
+
+    let mut full_mail : String = String::new();
+
+    let subject = choose_random_line(&subject_lines);
+
+    let greeting = choose_random_line(&greeting_lines);
+
+    let ending = choose_random_line(&ending_lines);
+
+    let content = choose_multiple_random_lines(&content_lines, 10);
+
+    full_mail.push_str(&subject);
+    full_mail.push_str("\n");
+    full_mail.push_str("\n");
+    full_mail.push_str(&greeting);
+    full_mail.push_str("\n");
+    for line in content {
+        full_mail.push_str(&line);
+        full_mail.push_str("\n");
+    }
+    full_mail.push_str("\n");
+    full_mail.push_str(&ending);
+
+    
+    Ok(full_mail)
 }
--- src/util.rs
+++ src/util.rs
@@ -9,7 +9,30 @@ pub fn read_file(file_path: &str) -> Result<String, Box<dyn Error>> {
 
 // Helper function to choose a random line
 pub fn choose_random_line(content: &str) -> String {
-    let lines: Vec<&str> = content.lines().collect();
+    let lines: Vec<&str> = content.lines()
+        .collect();
     let random_index = rand::random::<usize>() % lines.len();
-    lines[random_index].to_string()
+    lines[random_index].to_string().replace("\\n", "\n")
+}
+
+// Helper function to choose multiple random, non repeating lines
+pub fn choose_multiple_random_lines(content: &str, line_count: i32) -> Vec<String> {
+    let mut used_lines_indicies : Vec<usize> = Vec::new();
+    let mut chosen_lines : Vec<String> = Vec::new();
+
+    let lines : Vec<&str> = content.lines().collect();
+
+    for _i in 0..line_count {
+        let mut random_index : usize = rand::random::<usize>() % lines.len();
+
+        while used_lines_indicies.contains(&random_index) {
+            random_index = rand::random::<usize>() % lines.len();
+        }
+
+        used_lines_indicies.push(random_index);
+        chosen_lines.push(lines[random_index].to_string().replace("\\n", "\n"));
+
+    }
+
+    chosen_lines
 }
